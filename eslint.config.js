e6e6245c2b1ed97aa194e9dc86c4338157299a4b
SafeSpec-founder <donovansafespec@gmail.com>
2025-06-19 08:42:40 +0700

    Fix sign-in by marking new users active

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,9 @@
 const { FlatCompat } = require("@eslint/eslintrc");
+const { configs } = require("@eslint/js");
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: configs.recommended,
 });
 
 module.exports = [
